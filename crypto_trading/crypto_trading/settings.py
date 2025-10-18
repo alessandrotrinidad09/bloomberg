d7508031bffcd9ee1286ecaa15175837697f2dd5
Enrique <luisvalrc@gmail.com>
2025-10-18 11:07:21 -0500

    name BD

--- crypto_trading/crypto_trading/settings.py
+++ crypto_trading/crypto_trading/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bloomberg',
         'USER': 'root',
-        'PASSWORD': 'capstone123',
+        'PASSWORD': 'alianzalima1',
         'HOST': 'localhost',
         'PORT': '3306',
     }   
